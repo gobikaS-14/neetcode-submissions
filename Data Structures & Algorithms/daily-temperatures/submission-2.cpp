237d6c62f43354eef90c0b8acbaf698b90f67a1a
GOBIKASRI S <23z113@psgitech.ac.in>
2026-06-04 17:08:55 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& arr) {
+        int n= arr.size();
+        vector<int>res(n,0);
+        for(int i=0;i<n-1;i++){
+            int j=i;
+            while(j<n){
+                if(arr[j]<=arr[i]) j++;
+                else{
+                    res[i]=j-i;
+                    break;
+                }
+
+            }
+        }
+        return res;
+    }
+};
