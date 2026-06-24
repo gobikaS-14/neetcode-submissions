da4c07e22042e566e5c7014c13993c3998a8e096
GOBIKASRI S <23z113@psgitech.ac.in>
2026-06-24 08:48:51 +0530

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+       int n=grid.size();
+       int m=grid[0].size();
+       queue<pair<int,int>> q;
+
+       int dr[4] = {-1, 1, 0, 0};
+       int dc[4] = {0, 0, -1, 1};
+       int minutes =0;
+       int fresh=0; 
+
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2)   q.push({i,j});
+            else if (grid[i][j] == 1) fresh++;
+        }
+       }
+
+       while(!q.empty() && fresh>0){//when the grid has no rotten oranges and no fresh oranges.
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int r=q.front().first;
+                int c=q.front().second;
+                q.pop();
+                for(int k=0;k<4;k++){ 
+                    int nr=r+dr[k];
+                    int nc=c+dc[k];
+                    if(nr >= 0 && nr < n &&nc >= 0 && nc < m && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;//visited and rotted
+                        fresh--;
+                        q.push({nr,nc});
+                    }
+                }
+           }
+           minutes++;
+       }
+       return (fresh==0)? minutes:-1;
+    }
+};
