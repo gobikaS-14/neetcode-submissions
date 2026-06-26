266755c6c8f2f5a7dc2ac0fdadba35613b4e0289
GOBIKASRI S <23z113@psgitech.ac.in>
2026-06-26 08:44:49 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1)
+    return false;
+        vector<vector<int>> adj(n);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        //bfs
+        queue<pair<int,int>>q;
+        vector<int> v(n, 0);
+        q.push({0,0});
+        v[0]=1;
+        while(!q.empty()){
+            int curr=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            for(int neigh:adj[curr]){//neighs
+                if(v[neigh] &&  neigh!=parent){
+                    return false;//cycle exists
+                }
+                if(v[neigh]==0) {
+                v[neigh]=1;
+                q.push({neigh,curr});
+                }
+                
+            }
+        }
+        //check for all nodes are  visited(connectivity)
+        for (int i : v) {
+            if (!i) return false;
+        }
+
+        return true;
+    }
+};
