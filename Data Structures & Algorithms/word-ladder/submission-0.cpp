4e92da84f3999f54c00a5e2e2fa328caa2b5c5a5
GOBIKASRI S <23z113@psgitech.ac.in>
2026-06-28 18:43:36 +0530

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int ladderLength(string begin, string end, vector<string>& word) {
+        queue<pair<string,int>> q;
+        unordered_set<string> set(word.begin(),word.end());
+        q.push({begin,1});
+    
+        //bfs
+        while(!q.empty()){
+            auto[node,l]=q.front();
+            q.pop();
+            if(node==end) return l;
+            for(int i=0;i<node.size();i++){
+                string temp=node;
+                for(char c='a';c<='z';c++){
+                    temp[i]=c;
+                    if(set.count(temp)){
+                        set.erase(temp);
+                        q.push({temp,l+1});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
