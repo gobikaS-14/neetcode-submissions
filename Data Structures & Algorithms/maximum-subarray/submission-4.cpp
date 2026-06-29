df2344d3e51da4441c5dadda0fa91e43bc96052a
GOBIKASRI S <23z113@psgitech.ac.in>
2026-06-29 09:45:40 +0530

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.cpp
@@ -0,0 +1,17 @@
+
+//Greedy(kadane-o(n))
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int max_sum=INT_MIN;
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max_sum=max(sum,max_sum);
+            if(sum<0)   sum=0;
+        }
+        return max_sum;
+    }
+};
